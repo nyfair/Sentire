9086bcabbd160e4503265f50d417c6a1686be0a6
nyfair <nyfair2012@gmail.com>
2015-06-25 19:33:31 +0800

    drag-drop

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,15 @@ mod utils;
 extern crate libc;
 extern crate ini;
 extern crate sdl2;
-use sdl2::event::Event;
+use sdl2::event::{Event, EventType};
 use sdl2::rect::Rect;
+use sdl2::render::Renderer;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::surface::Surface;
 
-use std::env;
-
-fn main() {
-	let ext_map = utils::init_ext_map();
-	let fname = env::args().nth(1).unwrap();
-
+fn draw_iamge(fname: String, renderer: &mut Renderer) {
+	renderer.clear();
 	let img = fi::Bitmap::open(&fname);
 	let w = img.width();
 	let h = img.height();
@@ -23,20 +20,27 @@ fn main() {
 	let pitch = img.pitch();
 	let img_bits = img.raw();
 
+	let image = Surface::from_data(img_bits, w, h, pitch, PixelFormatEnum::BGR24).unwrap();
+    let texture = renderer.create_texture_from_surface(&image).unwrap();
+	renderer.copy_ex(&texture, None, Some(Rect::new_unwrap(0, 0, w, h)), 0.0, None, (false, true));
+	renderer.present();
+}
+
+fn main() {
+	let ext_map = utils::init_ext_map();
 	let mut sdl_context = sdl2::init().video().unwrap();
-	let window = sdl_context.window("sentire", w, h)
+	sdl_context.event_pump().enable_event(EventType::DropFile);
+	let window = sdl_context.window("sentire", 800, 600)
 		.position_centered()
 		.opengl()
 		.build()
 		.unwrap();
 	let mut renderer = window.renderer().build().unwrap();
-	let image = Surface::from_data(img_bits, w, h, pitch, PixelFormatEnum::BGR24).unwrap();
-    let texture = renderer.create_texture_from_surface(&image).unwrap();
-	renderer.copy_ex(&texture, None, Some(Rect::new_unwrap(0, 0, w, h)), 0.0, None, (false, true));
 	renderer.present();
 
 	for event in sdl_context.event_pump().wait_iter() {
 		match event {
+			Event::DropFile { filename: fname, .. } => draw_iamge(fname, &mut renderer),
 			Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break,
 			_ => {}
 		}
